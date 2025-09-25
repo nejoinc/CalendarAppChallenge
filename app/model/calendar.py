0c3c59cf7dc70f9e8482c201cedb93681562a9b5
nejoinc <josemanuelvalenciavelez@gmail.com>
2025-09-25 17:38:54 -0500

    Add Day and Calendar Classes with some methods

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -45,5 +45,62 @@ class Event:
         return f'ID: {self.id}\n Event title: {self.title}\n Description: {self.description}\n Time: {self.start_at} - {self.end_at}'
 
 
-print("prueba para git")
+class Day:
+    def __init__(self, date_: date):
+        self.date_: date = date_
+        self.slots: dict[time, str | None] = {}
+        self._init_slots()
+
+
+    def _init_slots(self):
+        self.slots: dict[time, str | None] = {}
+        for minutes in range(0, 24 * 60, 15):
+            hour = minutes // 60
+            minute = minutes % 60
+            self.slots[time(hour, minute)] = None
+
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        pass
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
+
+
+class Calendar:
+    def __init__(self):
+        self.days: dict[date, Day] = {}
+        self.events: dict[str, Event] = {}
+
+    def add_event(self, title: str, description: str, date_: date, start_at: time, end_at: time) -> str:
+        if date_ < datetime.now().date():
+            date_lower_than_today_error()
+        else:
+            if date_ not in self.days:
+                self.days[date_] = Day(date_)
+        new_event = Event(title, description, date_, start_at, end_at)
+
+        self.days[date_].add_event(new_event.id)
+        self.events[new_event.id] = new_event
+
+        return new_event.id
+
+
 
