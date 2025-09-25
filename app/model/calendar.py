2095b11323139cd757ceecc1b59a860928804a3c
david garcia <davidgarciavillanueva15@gmail.com>
2025-09-25 17:04:52 -0500

    Add Reminder and Event Classes with all methods

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -6,13 +6,43 @@ from app.services.util import generate_unique_id, date_lower_than_today_error, e
     reminder_not_found_error, slot_not_available_error
 
 
-# TODO: Implement Reminder class here
 
 
-# TODO: Implement Event class here
+@dataclass
+class Reminder:
+    EMAIL: ClassVar[str] = 'email'
+    SYSTEM: ClassVar[str] = 'system'
+
+    date_time: datetime
+    type: str = field(default=EMAIL)
+
+    def __str__(self):
+        return f'Reminder on {self.date_time} of type {self.type}'
 
 
-# TODO: Implement Day class here
+@dataclass
+class Event:
+    title: str
+    description: str
+    date_: date
+    start_at: time
+    end_at: time
+    reminders: list[Reminder]= field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_reminder(self, date_time: datetime, type: str):
+        self.reminders.append(Reminder(date_time, type))
 
 
-# TODO: Implement Calendar class here
+
+    def delete_reminder(self, reminder_index: int):
+        if 0 <= reminder_index < len(self.reminders):
+            del self.reminders[reminder_index]
+        else:
+            reminder_not_found_error()
+
+    def __str__(self):
+        return f'ID: {self.id}\n Event title: {self.title}\n Description: {self.description}\n Time: {self.start_at} - {self.end_at}'
+
+
+
